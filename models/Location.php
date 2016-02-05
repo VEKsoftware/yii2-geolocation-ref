d9a60c7cec0f51d4c25535096c8f6c3ba2d2b32b
Mikhail Eremeev <velonski@yandex.ru>
2016-02-05 14:46:00 +0500

    added findLocation() static method in Location model

--- models/Location.php
+++ models/Location.php
@@ -4,6 +4,8 @@ namespace geolocation\models;
 
 use Yii;
 
+use yii\helpers\ArrayHelper;
+
 /**
  * This is the model class for table "{{%location}}".
  */
@@ -13,14 +15,46 @@ class Location extends \geolocation\components\CommonRecord
      * list of location types
      */
     public static $types = [
-        'COUNTRY_GROUP' => 1,   // группа стран
-        'COUNTRY' => 2,         // страна
-        'REGION_GROUP' => 3,    // федеральный округ
-        'REGION' => 4,          // субъект федерации (область, республика, край)
-        'COUNTY' => 5,          // район области
-        'CITY' => 6,            // город
-        'STREET' => 7,          // улица
-        'HOUSE' => 8,           // дом
+        [
+            'id' => 1,
+            'name' => 'Группа стран',
+            'tag' => 'country_group',
+        ],
+        [
+            'id' => 2,
+            'name' => 'Страна',
+            'tag' => 'country',
+        ],
+        [
+            'id' => 3,
+            'name' => 'Федеральный округ',
+            'tag' => 'region_group',
+        ],
+        [
+            'id' => 4,
+            'name' => 'Субъект федерации', // область, республика, край
+            'tag' => 'region',
+        ],
+        [
+            'id' => 5,
+            'name' => 'Район',
+            'tag' => 'county',
+        ],
+        [
+            'id' => 6,
+            'name' => 'Город',
+            'tag' => 'city',
+        ],
+        [
+            'id' => 7,
+            'name' => 'Улица',
+            'tag' => 'street',
+        ],
+        [
+            'id' => 8,
+            'name' => 'Дом',
+            'tag' => 'house',
+        ],
     ];
     
     /**
@@ -97,4 +131,44 @@ class Location extends \geolocation\components\CommonRecord
         return $this->hasMany(Location::className(), ['id' => 'upper_id'])->via('locationLinksUpper');
     }
     
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public static function findLocation( $input )
+    {
+        if( !is_array($input) ) return null;
+        
+        $listTypes = ArrayHelper::map( static::$types, 'tag', 'id' );
+        
+        $filtered = array_filter( $input, function( $key ) use($listTypes) { return isset( $listTypes[$key] ); }, ARRAY_FILTER_USE_KEY );
+        if( empty($filtered) ) return null;
+        
+        $counter = 0;
+        $locationTable = Location::tableName();
+        
+        $query = static::find();
+        foreach( $filtered as $type => $name ) {
+            
+            if( $counter === 0 ) {
+                
+                $query->andWhere(['and', [$locationTable.'.type_id' => $listTypes[$type]], [$locationTable.'.name' => $name]]);
+                
+            } else {
+                
+                $linksTable = 'link'.strval($counter);
+                
+                $query->leftJoin(LocationLinks::tableName().' AS '.$linksTable, $linksTable.'.lower_id = '.$locationTable.'.id');
+                
+                $locationTable = 'loc'.strval($counter);
+                $query->leftJoin(Location::tableName().' AS '.$locationTable, $locationTable.'.id = '.$linksTable.'.upper_id');
+                
+                $query->andWhere(['and', [$locationTable.'.type_id' => $listTypes[$type]], [$locationTable.'.name' => $name]]);
+            }
+            
+            $counter++;
+            
+        }
+        
+        return $query->one();
+    }
 }
