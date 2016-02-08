74a5adfa3b6cb508ed1a80f92d2d3e98332447ea
Mikhail Eremeev <velonski@yandex.ru>
2016-02-08 16:56:49 +0500

    fixed Currency model alias

--- models/CurrencySearch.php
+++ models/CurrencySearch.php
@@ -7,7 +7,7 @@ use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 
-use wallets\models\Currency;
+use geolocation\models\Currency;
 
 /**
  * WalletsSearch represents the model behind the search form about `wallets\models\Wallets`.
